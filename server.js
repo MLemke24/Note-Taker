a36f17f00b43f26bcb8565dd3da4bc69ecd5f80b
mlemke24 <mattrlemke@gmail.com>
2021-04-21 09:04:19 -0500

    first push to heroku

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const notes = require('./db/db.json')
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+
+  //// http://localhost:3001/api/notes
